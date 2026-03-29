cdc06e70c2c4fa6498883843bfdb6e4618805fd7
Abhishek Mishra <sumitmm9506555798@gmail.com>
2026-03-30 00:30:40 +0530

    Chap-2: Implemented all methods to create Virtual thread

---
+++ secondChapter/PlatformThreadIntruption.java
@@ -0,0 +1,23 @@
+package secondChapter;
+
+public class PlatformThreadIntruption {
+   public static void main(String[] args)  {
+    Thread platformThread = Thread.ofPlatform().start(()->{
+        try {
+            System.out.println("Platform Thread Started...");
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Platform Thread Working : "+ i);
+                Thread.sleep(1000);
+            }
+            System.out.println("Platform Thread Finished...");
+        } catch (InterruptedException e) {
+            System.out.println("[[ WARNING ]] Platform Thread Intrupted ");
+        }
+    });
+    try {
+        Thread.sleep(2500);
+    } catch (InterruptedException e) {
+    }
+    platformThread.interrupt();
+   } 
+}
